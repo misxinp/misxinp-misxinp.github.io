cfbd3183fcb758d0e835b564e9d500d59ee605a9
zhangxp <zhangxp@dazhuanjia.com>
2020-08-28 15:55:14 +0800

    fix: delete TestRoutes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import HomeRoutes from './features/homepage/routes'
-import TestRoutes from './features/other/routes'
 
 const Routes = props => (
     <div>
         <HomeRoutes {...props} />
-        <TestRoutes {...props} />
     </div>
 )
 
